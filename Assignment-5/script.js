3bc1c96923179a96cff4f259f2818adeb07851bb
harrynova <saminova2001@gmail.com>
2026-04-11 00:49:18 +0500

    Programs

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -41,3 +41,80 @@ while (b <= 5);
     }
     console.log(row);
 }
+
+// Q5
+// ANS
+
+    for(let e=1; e<= 20; e++){
+        if(e%2===0){
+            continue;
+        }
+        console.log(e)
+    }
+
+// Q6
+// ANS: Function:  
+//                  Function is a block of code that performs 
+// specific task and can be used again whenever needed.
+
+//     function functionName(parameters)
+//          code execute
+//      }
+
+function add(a,b)
+{
+     return a+b;
+}
+
+    let result = add(5,7);
+    console.log(result);
+
+    // Q7
+    // ANS:
+
+    function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(isEven(4))
+console.log(isEven(7))
+
+// Q8
+// ANS
+
+function greet(name, message = "Hello") {
+    console.log(message + " " + name);
+}
+
+// Calling with both arguments
+greet("Ali", "Hi");
+
+// Calling with only one argument
+greet("Sara");
+
+// Q9
+// ANS: Function:
+//               A function expression is a function that is stored 
+// in a variable.
+
+    const multiply = function(a, b) {
+    return a * b;
+    }
+
+console.log(multiply(6, 3));
+
+// Q10
+// ANS:
+
+     function sayHello() {
+    console.log("Hello");
+}
+
+for (let i = 1; i <= 5; i++) {
+    sayHello();
+}
