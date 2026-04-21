75ce0d417e716f7b3e2e26bd46d2cb7a98dc73d8
harrynova <saminova2001@gmail.com>
2026-04-21 09:04:20 +0500

    java

---
+++ java lecture_3/Finals/java1.java
@@ -0,0 +1,36 @@
+class Student{
+    int id;
+    studyname;
+    class  BSCS{
+        int id;
+        string session;
+        BSCS(){
+        }
+    }
+    BSCS(){}
+    BSCS(int i)
+    {
+        id = u;
+    }
+    BSCS(string s)
+    {
+        session = s;
+    }
+    BSCS (int i){
+        id = u;
+    }
+    BSCS(i){}
+    BSCS(int i, string s){
+        session = s;
+        id  = i;
+    }
+    BSCS(string s, int i){
+        session = s;
+        id = i;
+    }
+    BSCS(int i, string s)
+    {
+        id = i;
+        session s;
+    }  
+}
