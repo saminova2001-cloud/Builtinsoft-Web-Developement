399b9dc7279562d1eced64a76a11d8da5fd41bc0
harrynova <saminova2001@gmail.com>
2026-04-24 12:49:34 +0500

    host issue resolve

--- React.js/appes/src/App.jsx
+++ React.js/appes/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import Navbar from './components/Navbar';
+// import Navbar from './components/ProjectJdot';
+import './Netflix.css'
 import ProjectNetflix from './ProjectNetflix';
 
 function App() {
   return(
     <div>
-      {/* <Navbar/> */}
+      {/* <ProjectJdot/> */}
       {/* <h1>Helloooooooo</h1> */}
       <ProjectNetflix/>
     </div>
