b99b05a827323affc4f0c6bd2cfb85d790771fe3
harrynova <saminova2001@gmail.com>
2026-04-27 18:53:18 +0500

    hookes

---
+++ React.js/appes/src/components/FpNav.jsx
@@ -0,0 +1,12 @@
+function FpNav() {
+    return (
+        <div className="navbar-1">
+            <img src="/images/panda-logo.png" alt="foodpanda logo" />
+            <div className="foodpanda">
+                <img className="foodpanda" src="/images/foodpaanda.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default FpNav
---
+++ React.js/appes/src/practice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class practice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello  world</h1>
+      </div>
+    )
+  }
+}
