79556c2aecea3d2032741df589c0f6dcf986afdd
harrynova <saminova2001@gmail.com>
2026-04-28 18:47:00 +0500

    foodpanda

--- React.js/appes/src/components/FpNav.jsx
+++ React.js/appes/src/components/FpNav.jsx
@@ -1,3 +1,4 @@
+// import {Location} from "react-icons/fa";
 function FpNav() {
     return (
         <div className="navbar-1">
@@ -5,7 +6,17 @@ function FpNav() {
             <div className="foodpanda">
                 <img className="foodpanda" src="/images/foodpaanda.png" alt="" />
             </div>
+            <div className="loca">
+                <p>New address Select your address</p>
+            </div>
+            <div className="right">
+                <p>harry</p>
+            </div>
+            <div className="lang">
+                <p>EN</p>
+            </div>
         </div>
+        
     )
 }
 
